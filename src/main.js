116c356fc3f4a2156b6fdf747e064706278861ed
Edgar Tamez <e.tamez25@hotmail.com>
2020-03-09 12:54:51 -0600

    Acceso a los request

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+// Vue.http.options.root = 'http://localhost:80/ProyectosGit/petbarbershop/api2/public/'
+
+Vue.http.interceptors.push((request, next) => {
+  request.headers.set('Accept', 'application/json')
+  next()
+});
+
 new Vue({
   router,
   store,
