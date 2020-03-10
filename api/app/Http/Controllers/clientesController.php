304268bb9b6df5bc9ea2c6152336dc5e1fcb6c47
Edgar Tamez <e.tamez25@hotmail.com>
2020-03-10 12:04:32 -0600

    Rename File clientes

--- api/app/Http/Controllers/clientesController.php
+++ api/app/Http/Controllers/clientesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\clientes;
+use App\clientesModel;
 
 class clientesController extends Controller
 {
--- api/app/clientesModel.php
+++ api/app/clientesModel.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class clientes extends Model
+class clientesModel extends Model
 {
     protected $table = "clientes";
     protected $fillable = array('nombre',
