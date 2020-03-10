b6253c47c02a39fb7e86bb96f16110989cf7c4bd
Edgar Tamez <e.tamez25@hotmail.com>
2020-03-10 12:23:50 -0600

    returno a nombres originales
    Al momento de renombrar todos los archivos se genera un error que no logre detectar.

--- api/app/Http/Controllers/clientesController.php
+++ api/app/Http/Controllers/clientesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\clientesModel;
+use App\clientes;
 
 class clientesController extends Controller
 {
--- api/app/clientes.php
+++ api/app/clientes.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class clientesModel extends Model
+class clientes extends Model
 {
     protected $table = "clientes";
     protected $fillable = array('nombre',
