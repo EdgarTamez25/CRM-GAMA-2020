e78ef96c8fe9528d723296c93b0e0499a347ef80
Angel-Escajeda <01100001.01100101@protonmail.com>
2021-10-25 17:45:58 -0500

    unidadesController

--- api/app/Http/Controllers/unidadesController.php
+++ api/app/Http/Controllers/unidadesController.php
@@ -8,28 +8,32 @@ use App\unidades;
 
 class unidadesController extends Controller
 {
-    public function getAll()
+
+    public function obtenerUnidades()
     {
-        $Unidad = unidades::all();
-        return $Unidad;
+        return DB::select('SELECT * FROM unidades WHERE estatus = 1');
     }
 
-		public function add(Request $request){
-			$addunidad = unidades::create($request->all());
-			
-			if($addunidad):
-				return "La Unidad se ah insertado correctamente";
-			else:
-				return "Ocurrio un problema al crear la unidad, por favor intentelo mas tarde.";
-			endif;
+    public function agregarUnidades(Request $request){
+        $unidades = DB::table('unidades')->insertGetId(
+                [
+                    'nombre' 		    => $req -> nombre,
+                    'estatus'  		    => $req -> estatus
+                ]
+        );
+        $unidades ? response("La unidad se creo Correctamente",200):
+        response("Ocurrio un error por favor intentelo de nuevo", 500);
+    }
 
-		}
-		
-		public function update($id, Request $req){
-			$data = DB::update('UPDATE unidades SET nombre=:nombre, estatus=:estatus WHERE id =:id',
-													['nombre'=> $req -> nombre,'estatus'=> $req -> estatus, 'id'=> $id	]
-												);
-			
-			return 'La Unidad se actualizo correctamente';
-	}
+    public function actualizarUnidades($id, Request $req){
+        $data = DB::update('UPDATE unidades SET nombre=:nombre, estatus=:estatus
+                                            WHERE id =:id',
+                                            [
+                                            'nombre'=> $req -> nombre,
+                                            'estatus'=> $req -> estatus,
+                                            'id'=> $id
+                                            ]);
+	return $data ?  response("La unidad se actualizo Correctamente",200):
+				    response("Ocurrio un error, vuelve a intentarlo mas tarde", 500);
+    }
 }
