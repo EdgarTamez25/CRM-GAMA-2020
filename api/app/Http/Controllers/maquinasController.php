1112446529c1a400f36e57a03933c8693f4f672a
Angel-Escajeda <01100001.01100101@protonmail.com>
2021-10-26 12:27:33 -0500

    INIT

--- api/app/Http/Controllers/maquinasController.php
+++ api/app/Http/Controllers/maquinasController.php
@@ -8,29 +8,16 @@ use Illuminate\Support\Facades\DB;
 
 class maquinasController extends Controller
 {
-<<<<<<< HEAD
     public function MaquinasxDepto($id_depto){
         return DB::select('SELECT * FROM maquinas WHERE id_depto = ? AND estatus = 1',[$id_depto]);
     }
-=======
-    public function Maquinas(){
-        return DB::select('SELECT * FROM maquinas WHERE estatus = 1');
-    }
-		
->>>>>>> AE
 		public function ObtenerMaquinasxOp(Request $req){
 			$maquinasxOp = DB::select('SELECT mo.id,mo.id_maquina, m.nombre as maquina, mo.id_operador, u.nombre as operador, mo.fecha, mo.estatus
 																	FROM  maquinax_op mo LEFT JOIN maquinas m ON mo.id_maquina = m.id
 																											 LEFT JOIN users    u ON mo.id_operador = u.id
 																	WHERE fecha = ?',[ $req -> fecha]);
 			return $maquinasxOp ? $maquinasxOp : [];
-<<<<<<< HEAD
 		}
-=======
-
-		}
-
->>>>>>> AE
     public function AgregarMaquinasxOP(Request $req){
 			$maquinaxOp = DB::table('maquinax_op')->insertGetId(  
 					[
@@ -43,10 +30,6 @@ class maquinasController extends Controller
 			return $maquinaxOp ? response("Agregado Correctamente",200): 
 													 response("Ocurrio un error por favor intentelo de nuevo", 500);
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> AE
 		public function EditarMaquinasxOP(Request $req){ 
 			$editarMaquina = DB::update('UPDATE maquinax_op SET id_maquina=:id_maquina, id_operador=:id_operador, fecha=:fecha WHERE id=:id',
 													['id_maquina' => $req -> id_maquina,
@@ -58,7 +41,6 @@ class maquinasController extends Controller
 			return $editarMaquina? response('Se actualizo correctamente', 200): 
 														response("Ocurrio un error por favor intentelo de nuevo", 500);
 		}
-<<<<<<< HEAD
 		public function MaquinaAsignada(Request $req){
 			$prog_op = DB::select('SELECT * FROM  maquinax_op WHERE fecha = ? AND id_operador = ?',[ $req -> fecha, $req -> id_operador]);
 			return $prog_op ? $prog_op: response("No hay asignaciones para ti. Pide a tu encargado que te asigne a una estación",500);
@@ -79,31 +61,6 @@ class maquinasController extends Controller
 
 
 			return $programacion ? $programacion: response("No hay programaciones para está maquina",500);
-=======
-
-
-		public function MaquinaAsignada(Request $req){
-			$prog_op = DB::select('SELECT * FROM  maquinax_op WHERE fecha = ? AND id_operador = ?',[ $req -> fecha, $req -> id_operador]);
-			return $prog_op ? $prog_op: response("No hay asignaciones para ti. Pide a tu encargado que te asigne a una estación",500);
-
-		}
-
-		public function ProgramacionMaquina($id_maquina){
-			$programacion = DB::select('SELECT  f.id, f.fecha_prog,f.id_maquina, m.nombre as maquina, f.ot, f.num_partida,
-																					f.id_cliente, c.nombre as nomcli, f.id_producto, p.codigo as producto, f.cantidad,
-																					f.finalizacion,f.id_pleca, pl.dientes as pleca, f.id_suaje, s.dientes as suaje, 
-																					f.hora_inicio, f.hora_fin,f.urgencia, f.estatus FROM flexo_ot f 
-																		LEFT JOIN maquinas m  ON f.id_maquina  = m.id
-																		LEFT JOIN clientes c  ON f.id_cliente  = c.id 
-																		LEFT JOIN prodxcli p  ON f.id_producto = p.id
-																		LEFT JOIN plecas   pl ON f.id_pleca    = pl.id
-																		LEFT JOIN suajes   s  ON f.id_suaje    = s.id
-																		WHERE f.estatus = 1   AND f.id_maquina = ?',
-																		[ $id_maquina ]);
-
-			return $programacion ? $programacion: response("No hay programaciones para está maquina",500);
-			
->>>>>>> AE
 		}
 
 		public function EliminarAsignacion($id){
@@ -111,10 +68,6 @@ class maquinasController extends Controller
 			return $eliminaAsignacion ? response("Se elimino correctamente",200):
 															 response("Ocurrio un error amiguito, vuelve a intentarlo mas tarde", 500);
 		}
-<<<<<<< HEAD
-=======
-
->>>>>>> AE
 		public function ValidaConservacion(Request $req){
 			$validacion = DB::select('SELECT * FROM maquinax_op WHERE fecha = ?', [$req -> fecha1]);
 			return $validacion ? $validacion:[]; 
--- api/app/Http/Controllers/programacionFlexoController.php
+++ api/app/Http/Controllers/programacionFlexoController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\DB;
 class programacionFlexoController extends Controller
 {
 		public function obtenerProgramacion(Request $req){
-<<<<<<< HEAD
 			$fecha2 = $req -> fecha2 .' '. $req -> horaActual;
 			$programaciones = DB::select('SELECT f.id, f.id_det_ot, do.id_ot, ot.id_cliente, c.nombre as nomcli, 
 																					do.id_producto, p.codigo, do.cantidad, do.concepto, f.fecha_prog, f.id_maquina,
@@ -22,18 +21,6 @@ class programacionFlexoController extends Controller
 																		WHERE f.estatus = ? AND f.id_sucursal = ? AND f.creacion BETWEEN  ? AND ? 
 																		ORDER BY f.urgencia DESC, f.id DESC',
 																		[$req -> estatus , $req -> id_sucursal, $req -> fecha1 , $fecha2 ]);
-=======
-			$programaciones = DB::select('SELECT f.id, f.fecha_prog,f.id_maquina, m.nombre as maquina, f.ot, f.num_partida,
-																					 f.id_cliente, c.nombre as nomcli, f.id_producto, p.codigo as producto, f.cantidad,
-																					 f.finalizacion,f.id_pleca, f.id_suaje, s.dientes as suaje, 
-																					 f.hora_inicio, f.hora_fin, f.urgencia, f.estatus FROM flexo_ot f 
-																			LEFT JOIN maquinas m  ON f.id_maquina  = m.id
-																			LEFT JOIN clientes c  ON f.id_cliente  = c.id 
-																			LEFT JOIN prodxcli p  ON f.id_producto = p.id
-																			LEFT JOIN suajes   s  ON f.id_suaje    = s.id
-																		WHERE f.estatus = ? AND f.fecha_prog BETWEEN ? AND ? ORDER BY f.urgencia DESC, f.id DESC ',
-																		[$req -> estatus , $req -> fecha1 , $req -> fecha2 ]);
->>>>>>> AE
 
 			return $programaciones ? $programaciones : [];
 		}
@@ -63,7 +50,6 @@ class programacionFlexoController extends Controller
     }
 		
 		public function actualizaProgramacion($id, Request $req){
-<<<<<<< HEAD
 			$putPermiso = DB::update('UPDATE flexo_ot SET fecha_prog=:fecha_prog, id_maquina=:id_maquina,id_pleca=:id_pleca,
 																									 id_suaje=:id_suaje, hora_inicio=:hora_inicio, hora_fin=:hora_fin,
 																									 estatus=:estatus
@@ -72,19 +58,6 @@ class programacionFlexoController extends Controller
                                 'id_pleca'    => $req -> id_pleca     , 'id_suaje'    => $req -> id_suaje, 
                                 'hora_inicio' => $req -> hora_inicio  , 'hora_fin'    => $req -> hora_fin,     
 																'estatus'     => $req -> estatus      , 'id'          => $id	
-=======
-			$putPermiso = DB::update('UPDATE flexo_ot SET fecha_prog=:fecha_prog, id_maquina=:id_maquina, ot=:ot, num_partida=:num_partida,
-                                                   id_cliente=:id_cliente,id_producto=:id_producto, cantidad=:cantidad,id_pleca=:id_pleca,
-																									 id_suaje=:id_suaje, hora_inicio=:hora_inicio, hora_fin=:hora_fin, urgencia=:urgencia
-                                	WHERE id=:id',
-                              [ 'fecha_prog'	=> $req -> fecha_prog   , 'id_maquina'	=> $req -> id_maquina,
-                                'ot' 					=> $req -> orden_trabajo, 'num_partida' => $req -> num_partida,
-                                'id_cliente'  => $req -> id_cliente   , 'id_producto' => $req -> id_producto,
-                                'cantidad'    => $req -> cantidad     , 'id_pleca'    => $req -> id_pleca,
-                                'id_suaje'    => $req -> id_suaje     , 'hora_inicio' => $req -> hora_inicio,
-																'hora_fin'    => $req -> hora_fin     , 'urgencia'    => $req -> urgencia,
-																'id'          => $id	
->>>>>>> AE
                               ]
 												);
     	return $putPermiso ? response("La programación se actualizo correctamente",200):
@@ -146,22 +119,13 @@ class programacionFlexoController extends Controller
 		}
 
 		public function actualizarFlexoOT($id, $fin){
-<<<<<<< HEAD
 			return DB::update('UPDATE flexo_ot SET estatus=:estatus WHERE id=:id',
                               [ 'estatus' => 3, 'id'=> $id ]
-=======
-			return DB::update('UPDATE flexo_ot SET finalizacion=:finalizacion, estatus=:estatus WHERE id=:id',
-                              [ 'finalizacion'	=> $fin, 'estatus' => 3, 'id'=> $id ]
->>>>>>> AE
 												);
 		}
 
 		public function cancelarOTFlexo($id){
-<<<<<<< HEAD
 			$cancelacion = DB::update('UPDATE flexo_ot SET estatus=:estatus WHERE id=:id',['estatus' => 4 , 'id' => $id]);
-=======
-			$cancelacion = DB::update('UPDATE flexo_ot SET estatus=:estatus WHERE id=:id',['estatus' => 2 , 'id' => $id]);
->>>>>>> AE
 			return $cancelacion? response('Se cancelo correctamente',200): response('Ocurrio un error, intentelo mas tarde', 500);
 		}
 }	
