87617590c116cae85efb7d1848c1485d1d85c3d5
Angel-Escajeda <01100001.01100101@protonmail.com>
2021-11-22 17:22:49 -0600

    produccionController funcional

--- api/app/Http/Controllers/produccionController.php
+++ api/app/Http/Controllers/produccionController.php
@@ -13,34 +13,24 @@ class produccionController extends Controller{
     }
 	// ! ************************************************************************
 
-<<<<<<< refs/remotes/origin/ET
-	// **************** FUNCIONES AGREGAR PROGRAMACION **************************
-		public function ciclaProgramacion(Request $req){
-				// return ($req->programacion[0]['depto']['id']);
-				for ($i = 0; $i < count($req->programacion); $i++):
-						$id_produccion = $this->agregarProgramacion($req, $req->programacion[$i]['depto']['id'], $req->programacion[$i]['sucursal']['id']);
-						$this->agregarPrimerMov($req->programacion[$i], $id_produccion, $req->id_producto, $req->creacion);
-												$this->actualizar_det_ot($req -> id_det_ot, 2);  // ACTUALIZAR DET_OT 
-				endfor;
-				return response("Se programó correctamente", 200);
-		}
-=======
-    public function ciclaProgramacion(Request $req)
-    {
-        // return ($req->programacion[0]['depto']['id']);
-        for ($i = 0; $i < count($req->programacion); $i++):
-            $id_produccion = $this->agregarProgramacion($req, $req->programacion[$i]['depto']['id'], $req->programacion[$i]['sucursal']['id']);
-            $this->agregarPrimerMov($req->programacion[$i], $id_produccion, $req->id_producto, $req->creacion);
-        endfor;
-        $actualizarFecha = actualizaFechaProg($req->programacion);
-    }
+        public function ciclaProgramacion(Request $req)
+        {
+                // return ($req->programacion[0]['depto']['id']);
+                for ($i = 0; $i < count($req->programacion); $i++):
+                        $id_produccion = $this->agregarProgramacion($req, $req->programacion[$i]['depto']['id'], $req->programacion[$i]['sucursal']['id']);
+                        $this->agregarPrimerMov($req->programacion[$i], $id_produccion, $req->id_producto, $req->creacion);
+                                                $this->actualizar_det_ot($req -> id_det_ot, $req -> creacion, 2);  // ACTUALIZAR DET_OT
+                endfor;
+                return response("Se programó correctamente", 200);
+        }
 
-    public function actualizaFechaProg($data){
+    public function actualizaFechaProg($id_det_ot, $creacion, $estatus){
         $actualizaFechaProg = DB::update('UPDATE det_ot SET fecha_progra=:fecha_progra
                                           WHERE  id=:id',
                                           [
-                                              'fecha_progra'  => $data -> creacion,
-                                              'id'            => $data -> id_det_ot,
+                                              'fecha_progra'  => $creacion,
+                                              'estatus'       => $estatus,
+                                              'id'            => $id_det_ot
                                           ]);
     }
 
@@ -74,7 +64,6 @@ class produccionController extends Controller{
         DB::update('UPDATE det_ot SET estatus=:estatus, finalizacion=:finalizacion
                     WHERE id=:id',['estatus' => $data['estatus'],$fecha_actual], $data['id']);
     }
->>>>>>> actualizar estatus de_ot
 
 		public function actualizar_det_ot($id_det_ot, $estatus){
 				DB::update('UPDATE det_ot SET estatus=:estatus WHERE id=:id',['estatus' => $estatus, 'id' => $id_det_ot]);
@@ -84,9 +73,6 @@ class produccionController extends Controller{
 		public function agregarProgramacion($data, $id_depto, $id_sucursal){
 				// !    EL REQUEST CONTIENE UN ARRAY DE OBJETOS LLAMADO PROGRAMACION "$req -> programacion"
 				// ! 1. CREO EL REGISTRO PARA LA PRODUCCION
-				// return $item['sucursal'];
-				// return $item['sucursal'] -> id;
-				// return 'Sucursal: ' + $item['sucursal']['id'];
 				$agregarProg = DB::table('produccion')->insertGetId(
 						[
 								'id_det_ot' => $data['id_det_ot'],             //! 2 RECUPERO id_det_ot del data
@@ -156,10 +142,9 @@ class produccionController extends Controller{
     }
 
     public function obtener_datos_produccion(Request $req){
-<<<<<<< refs/remotes/origin/ET
 
         $produccion = DB::select('SELECT m.*,
-																				us.nombre  as nomemisor,  
+																				us.nombre  as nomemisor,
 																				us2.nombre as nomreceptor,
 																				d.nombre   as deptoemisor,
 																				d2.nombre  as deptoreceptor,
@@ -181,22 +166,6 @@ class produccionController extends Controller{
 																				LEFT JOIN det_ot        dt ON p.id_det_ot = dt.id
 																				LEFT JOIN ot  		   	     ON dt.id_ot = ot.id
 																				LEFT JOIN clientes       c ON ot.id_cliente = c.id
-=======
-        $produccion = DB::select('SELECT m.*,
-                                        a.codigo,
-                                        u.nombre as nomunidad,
-                                        p.fecha_entrega, p.urgencia,
-                                        dt.id_ot,
-                                        ot.id_cliente,
-                                        c.nombre as nomcli
-                                FROM movim_prod m
-                                        LEFT JOIN prodxcli   a ON m.id_producto   = a.id
-                                        LEFT JOIN unidades   u ON a.id_unidad     = u.id
-                                        LEFT JOIN produccion p ON m.id_produccion = p.id
-                                        LEFT JOIN det_ot dt    ON p.id_det_ot     = dt.id
-                                        LEFT JOIN ot  		   ON dt.id_ot        = ot.id
-                                        LEFT JOIN clientes  c  ON ot.id_cliente   = c.id
->>>>>>> actualizar estatus de_ot
                                 WHERE m.id_depto   = ? AND
                                         m.estatus_prod = ? AND
                                         m.creacion BETWEEN ? AND ?',
@@ -208,19 +177,18 @@ class produccionController extends Controller{
                                         ]);
 				return $produccion ? $produccion: [];
     }
-<<<<<<< refs/remotes/origin/ET
-		
+
 		public function iniciar_partida_movim(Request $req){
 			$iniciar_partida = DB::update('UPDATE movim_prod SET fecha_inicio=:fecha_inicio, estatus_prod=:estatus_prod
 																					WHERE id =:id',['fecha_inicio' => $req  ->  fecha_inicio,
 																													'estatus_prod' => $req  ->  estatus_prod,
 																													'id' 		 			 => $req  -> id_movim ]);
 
-			return $iniciar_partida? response("La partida se inicio correctamente",200): 
+			return $iniciar_partida? response("La partida se inicio correctamente",200):
 															 response("Ocurrio un error, intentelo de nuevo.",500);
-			
+
 		}
-	
+
 
 	// **************** AUTORIZAR ENVIO DE PRODUCTO *****************************
 		public function autorizar_envio_material(Request $req){
@@ -229,18 +197,18 @@ class produccionController extends Controller{
 				[
 					'id_produccion' => $req -> id_produccion,
 					'id_depto' 		  => $req -> id_nuevo_depto,
-					'id_sucursal'   => $req -> id_sucursal, 
-					'id_producto'   => $req -> id_producto,         
+					'id_sucursal'   => $req -> id_sucursal,
+					'id_producto'   => $req -> id_producto,
 					'cant_sol'      => $req -> cant_sol,
 					'tipo_prog'     => $req -> tipo_prog,
 					'id_usr_emisor' => $req -> id_usr_emisor,
 					'emisor'        => $req -> emisor,
 					'enviadas'      => $req -> enviadas,
-					'creacion' 			=> $req -> creacion,   
-					'estatus_prod'  => $req -> estatus_prod,            
+					'creacion' 			=> $req -> creacion,
+					'estatus_prod'  => $req -> estatus_prod,
 				]
 			);
-			
+
 			$this -> actualiza_movim_anterior($req -> id_movim, $req -> enviadas);
 
 			return $id_movim_prod? response("El producto se ha envíado correctamente.",200):
@@ -262,12 +230,12 @@ class produccionController extends Controller{
 		public function autorizar_recibo_material(Request $req){
 			$recibo_material = DB::update('UPDATE movim_prod
 																			SET
-																							id_usr_receptor=:id_usr_receptor, 
-																							receptor=:receptor,           
-																							recibidas=:recibidas,                
-																							estatus_prod=:estatus_prod       
+																							id_usr_receptor=:id_usr_receptor,
+																							receptor=:receptor,
+																							recibidas=:recibidas,
+																							estatus_prod=:estatus_prod
 																			WHERE   id=:id',
-							[	
+							[
 								'id_usr_receptor' => $req -> id_usr_receptor,
 								'receptor' 			  => $req -> receptor,
 								'recibidas' 			=> $req -> recibidas,
@@ -278,12 +246,12 @@ class produccionController extends Controller{
 																response("Ocurrio un problema , por favor intentelo mas tarde.", 500);
 		}
 	// ! ************************************************************************
-	
+
 
 	// **************** FINALIZACION DE MOVIMIENTO ******************************
 		public function finalizar_partida_movim(Request $req){
 			$actualizar = DB::update('UPDATE movim_prod
-                                    SET finalizacion=:finalizacion,       
+                                    SET finalizacion=:finalizacion,
                                         estatus_prod=:estatus_prod
                                     WHERE   id=:id',
             [
@@ -301,7 +269,7 @@ class produccionController extends Controller{
 	// **************** PRODUCTOS ENVIADOS **************************************
 		public function obtener_productos_enviados(Request $req){
 			$prod_enviados =  DB::select('SELECT m.*,
-																					us.nombre  as nomemisor,  
+																					us.nombre  as nomemisor,
 																					us2.nombre as nomreceptor,
 																					d.nombre   as deptoemisor,
 																					d2.nombre  as deptoreceptor,
@@ -334,17 +302,17 @@ class produccionController extends Controller{
 				return $prod_enviados ? $prod_enviados: [];
 		}
 	// ! ************************************************************************
-	
+
 
 	// **************** VALIDACION DE MOVIMIENTOS *******************************
 		public function validar_estatus_movimientos($data){
 			$cero=0;$uno=0; $dos=0; $tres=0; $cuatro=0; $movim=[];
 
-			$movim = DB::select('SELECT * FROM movim_prod 
+			$movim = DB::select('SELECT * FROM movim_prod
 														WHERE id_produccion =? AND id_producto = ?',
 													[$data -> id_produccion, $data -> id_producto]);
 
-			for($i=0;$i<count($movim); $i++): 
+			for($i=0;$i<count($movim); $i++):
 				if($movim[$i] -> estatus_prod === 0 ):
 					$cero++;
 				elseif($movim[$i] -> estatus_prod === 1):
@@ -355,7 +323,7 @@ class produccionController extends Controller{
 					$tres++;
 				endif;
 			endfor;
-			
+
 			if($cero > 0):
 				$objetTemp  = ["id" => $data['id_produccion'], "estatus" => 1 ];
 				$this -> actualizar_estatus_produccion($objetTemp);
@@ -377,13 +345,11 @@ class produccionController extends Controller{
 		 DB::update('UPDATE produccion SET finalizacion=:finalizacion, estatus=:estatus
 									WHERE id =:id',['finalizacion' => $fecha_actual,
 																	'estatus' => $data['estatus'],
-																	'id' => $data['id'] 
+																	'id' => $data['id']
 																 ]);
 		}
 	// ! ************************************************************************
 
 
 
-=======
->>>>>>> actualizar estatus de_ot
 }
